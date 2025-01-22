563e2cd59c77776f25d6e93ac006c5f1d2ad6d64
Ying Chen <106372+wing2fly@users.noreply.github.com>
2025-01-21 18:04:17 -0800

    Create libxmlsec1@1.2.41.rb

---
+++ Formula/libxmlsec1@1.2.41.rb
@@ -0,0 +1,21 @@
+class Libxmlsec1AT1241 < Formula
+  desc "XML Security Library"
+  homepage "https://www.aleksey.com/xmlsec/"
+  url "https://www.aleksey.com/xmlsec/download/xmlsec1-1.2.41.tar.gz"
+  sha256 "shasum -a 256 xmlsec1-1.2.41.tar.gz"
+  license "MIT"
+
+  depends_on "libxml2"
+  depends_on "libtool"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/xmlsec1", "--version"
+  end
+end
