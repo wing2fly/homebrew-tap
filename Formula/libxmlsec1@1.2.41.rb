7b739fe5642d3c4056c436e0a6970470f9e1fe68
Ying Chen <106372+wing2fly@users.noreply.github.com>
2025-01-21 18:10:52 -0800

    Update libxmlsec1@1.2.41.rb

--- Formula/libxmlsec1@1.2.41.rb
+++ Formula/libxmlsec1@1.2.41.rb
@@ -2,7 +2,7 @@ class Libxmlsec1AT1241 < Formula
   desc "XML Security Library"
   homepage "https://www.aleksey.com/xmlsec/"
   url "https://www.aleksey.com/xmlsec/download/xmlsec1-1.2.41.tar.gz"
-  sha256 "shasum -a 256 xmlsec1-1.2.41.tar.gz"
+  sha256 "a0aecfdf1f190c6b866a278e42746b6582729a493f6ac6a1556a4663ff3ce625"
   license "MIT"
 
   depends_on "libxml2"
